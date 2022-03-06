36a2c34434f0919bec7e04ae003bf07226596458
mule2020 <61051437+mule2020@users.noreply.github.com>
2022-03-06 14:07:37 +0530

    Update settings.py

--- loginsystem/settings.py
+++ loginsystem/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-3w&6(0!%@0^qmqs)e49r9f4o%ec7z$d&=a0&gty++hbz6qf!!n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
